db8a381c785c247f20ab233ae617a5c005b9b494
garrettwilliams90 <89825231+garrettwilliams90@users.noreply.github.com>
2021-12-17 21:00:00 -0500

    Added My Work

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,17 +1,47 @@
 class ShoppingCart:
     # write your code here
     def __init__(self, emp_discount=None):
-      pass
+        self.total = 0
+        self.employee_discount = emp_discount
+        self.items = []
+        
     def add_item(self, name, price, quantity=1):
-       pass
+        for i in range(quantity):
+            self.items.append({"name":name,"price":price})
+            self.total = self.total + price
+        return self.total
+    
     def mean_item_price(self):
-       pass
+        mean_ = (self.total)/(len(self.items))
+        return mean_
 
     def median_item_price(self):
-        pass
+        from operator import itemgetter
+        newlist_ = sorted(self.items, key = itemgetter('price'))
+        if len(newlist_)%2 == 1:
+            return newlist_[np.rint(len(newlist_)/2).astype(int)]['price']
+        else:
+            first_ = newlist_[int(len(newlist_)/2)]['price']
+            second_ = newlist_[int((len(newlist_)/2)-1)]['price']
+            return (first_ + second_)/2
 
     def apply_discount(self):
-       pass
+        if self.employee_discount == 0 or self.employee_discount is None:
+            return "Sorry, there is no discount to apply to your cart :("
+        else:
+            return self.total - (self.total*(self.employee_discount/100))
 
     def void_last_item(self):
-        pass
+        if len(self.items) == 0:
+            return "There are no items in your cart!"
+        else:
+            self.total = self.total - self.items[-1]['price']
+            self.items.pop()
+            return self.total
+        #SOLUTION
+#         if self.items:
+#             removed_item = self.items.pop()
+#         else:
+#             return "There are no items in your cart!"
+#         self.total -= removed_item['price']
+            
